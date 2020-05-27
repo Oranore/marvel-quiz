39a4f271ed9e4f42fd3107ed5e072fc963cc16a8
Oranore <60654293+Oranore@users.noreply.github.com>
2020-05-27 10:03:07 +0200

    Add appli marvel

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Ben - 2020</p>
+                <p>Icones trouvées sur iconFinder.com</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer
